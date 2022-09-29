5178000cbda541c74819419e8e72e6b2447f8d67
Jakub Czekalski <j.r.czekalski@gmail.com>
2022-09-29 23:40:28 +0200

    Have worked on the functionality of the options

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ RULESBUTTON.addEventListener('click', () => {
     RULESPOPUP.classList.remove('disactive');
 });
 
-const CLOSEPOPUP = (event) => {
+
+RULESPOPUP.addEventListener('click', (event) => {
     const CLOSE = RULESPOPUP.querySelector('.rules_popup-screen div button');
 
     if (event.target === CLOSE) {
@@ -13,6 +14,36 @@ const CLOSEPOPUP = (event) => {
     } else if (event.target.contains(RULESPOPUP)) {
         RULESPOPUP.classList.add('disactive')
     }
-};
+});
 
-RULESPOPUP.addEventListener('click', CLOSEPOPUP);
+// Choose option
+
+const GAME = document.querySelector('.gameBoard');
+const BATTLE = document.querySelector('.battleBoard')
+
+GAME.addEventListener('click', (event) => {
+    if (!event.target.closest('button')) return
+    const OPTION = event.target.closest('button')
+    const {type} = OPTION.dataset
+
+    if (OPTION) {
+        GAME.classList.add('disactive');
+        BATTLE.classList.remove('disactive');
+    }
+
+    const SCISSORS = BATTLE.querySelector('.scissors-battle');
+    const ROCK = BATTLE.querySelector('.rock-battle');
+    const PAPER = BATTLE.querySelector('.paper-battle');
+
+    if (type === 'scissors'){
+        SCISSORS.classList.remove('disactive')
+    };
+
+    if (type === 'rock'){
+        ROCK.classList.remove('disactive')
+    };
+
+    if (type === 'paper'){
+        PAPER.classList.remove('disactive')
+    };
+});
